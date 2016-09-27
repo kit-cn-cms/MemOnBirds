adb4144406e1b4760ccb0be048735e4328ecdb76
Karim <karim.morabit@kit.edu>
2016-09-27 10:02:31 +0200

    init

---
+++ Definitions.py
@@ -0,0 +1,13 @@
+#set up everything here
+
+# edit
+MaxEventsPerTree=200
+# edit
+OutputDirectoryForMEMTrees="/nfs/dust/cms/user/kelmorab/MEMInputTrees80x_newBTags"
+
+InputDirectoryOfMEMTrees="/nfs/dust/cms/user/kelmorab/MEMInputTrees80x_newBTags/addedTrees/"
+
+.
+Samples=[
+	["ttHbb_nominal","ttHbb_nominal.root",200,],
+]
